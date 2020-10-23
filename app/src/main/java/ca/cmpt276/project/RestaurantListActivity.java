bb31110e2d2f63e41c4e6197d6994b74599dcc8c
gloryq <gloryquan2@gmail.com>
2020-10-23 14:30:19 -0700

    initial project setup

---
+++ app/src/main/java/ca/cmpt276/project/RestaurantListActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+    }
+}
