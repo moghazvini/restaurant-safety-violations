d919e1cd8a08669a39d31d233db6ca4256574c6c
gloryq <gloryquan2@gmail.com>
2020-10-23 15:59:49 -0700

    setup restaurant class

--- app/src/main/java/ca/cmpt276/project/model/Restaurant.java
+++ app/src/main/java/ca/cmpt276/project/model/Restaurant.java
@@ -1,4 +1,66 @@
 package ca.cmpt276.project.model;
 
 public class Restaurant {
+    private String name;
+    private String address;
+    private float gpsLong;
+    private float gpsLat;
+    private String tracking;
+    private InspectionListManager inspections;
+
+    public Restaurant(String name, String address, float gpsLong, float gpsLat, String tracking) {
+        this.name = name;
+        this.address = address;
+        this.gpsLong = gpsLong;
+        this.gpsLat = gpsLat;
+        this.tracking = tracking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public float getGpsLong() {
+        return gpsLong;
+    }
+
+    public void setGpsLong(float gpsLong) {
+        this.gpsLong = gpsLong;
+    }
+
+    public float getGpsLat() {
+        return gpsLat;
+    }
+
+    public void setGpsLat(float gpsLat) {
+        this.gpsLat = gpsLat;
+    }
+
+    public String getTracking() {
+        return tracking;
+    }
+
+    public void setTracking(String tracking) {
+        this.tracking = tracking;
+    }
+
+    public InspectionListManager getInspections() {
+        return inspections;
+    }
+
+    public void setInspections(InspectionListManager inspections) {
+        this.inspections = inspections;
+    }
 }
