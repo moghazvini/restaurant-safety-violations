5eec8bfb11c6129fe0487a074f54c69b7f2b9fac
gloryq <gloryquan2@gmail.com>
2020-10-23 15:40:52 -0700

    setup inspection list manager class

--- app/src/main/java/ca/cmpt276/project/model/InspectionListManager.java
+++ app/src/main/java/ca/cmpt276/project/model/InspectionListManager.java
@@ -1,4 +1,35 @@
 package ca.cmpt276.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InspectionListManager {
+    private final List<Inspection> inspections;
+    private static InspectionListManager instance;
+
+    private InspectionListManager() {
+        inspections = new ArrayList<>();
+    }
+
+    public static InspectionListManager getInstance() {
+        if (instance == null) {
+            instance = new InspectionListManager();
+        }
+        return instance;
+    }
+
+    public void add(Inspection inspection) {
+        inspections.add(inspection);
+    }
+
+    public List<Inspection> getInspections() {
+        return inspections;
+    }
+
+    public Inspection getInspection(int index) {
+        if (index < 0 || index >= inspections.size()) {
+            return null;
+        }
+        return inspections.get(index);
+    }
 }
