281a959a9016699e5ef8da5099dbc0fda4814aef
gloryq <gloryquan2@gmail.com>
2020-10-23 15:19:44 -0700

    setup restaurant list manager class

--- app/src/main/java/ca/cmpt276/project/model/RestaurantListManager.java
+++ app/src/main/java/ca/cmpt276/project/model/RestaurantListManager.java
@@ -1,4 +1,35 @@
 package ca.cmpt276.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestaurantListManager {
+    private final List<Restaurant> restaurants;
+    private static RestaurantListManager instance;
+
+    private RestaurantListManager() {
+        restaurants = new ArrayList<>();
+    }
+
+    public static RestaurantListManager getInstance() {
+        if (instance == null) {
+            instance = new RestaurantListManager();
+        }
+        return instance;
+    }
+
+    public void add (Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
+
+    public List<Restaurant> getList() {
+        return restaurants;
+    }
+
+    public Restaurant getRestaurant(int index) {
+        if (index < 0 || index >= restaurants.size()) {
+            return null;
+        }
+        return restaurants.get(index);
+    }
 }
