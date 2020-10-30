810a0224ec5cc86757dde067fd3167b72836f8bf
gloryq <gloryquan2@gmail.com>
2020-10-29 17:41:57 -0700

    Set up activity for restaurant details

---
+++ app/src/main/java/ca/cmpt276/project/ui/RestaurantDetails.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.project.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.project.R;
+
+public class RestaurantDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_details);
+    }
+}
