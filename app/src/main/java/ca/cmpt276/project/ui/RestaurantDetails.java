fe5604df34a8b5588fe1c32b6610e06daa1e98d0
vaibhavs <vaibhavs@sfu.ca>
2020-10-31 02:49:48 -0700

    Getting some problems with list view...
    I have created the restaurant detail activity but I am getting some issues with displaying inspections list

--- app/src/main/java/ca/cmpt276/project/ui/RestaurantDetails.java
+++ app/src/main/java/ca/cmpt276/project/ui/RestaurantDetails.java
@@ -3,15 +3,23 @@ package ca.cmpt276.project.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import ca.cmpt276.project.R;
 import ca.cmpt276.project.model.Inspection;
+import ca.cmpt276.project.model.InspectionListManager;
 import ca.cmpt276.project.model.Restaurant;
 import ca.cmpt276.project.model.RestaurantListManager;
 
@@ -31,6 +39,7 @@ public class RestaurantDetails extends AppCompatActivity {
         Getdata();
         populateList();
         setValues();
+        OnClick();
     }
 
     private void populateList() {
@@ -47,10 +56,53 @@ public class RestaurantDetails extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View iv = convertView;
-            if(iv == null)
-        }
+            if(iv == null){
+                iv = getLayoutInflater().inflate(R.layout.inspection_list, parent, false);
+            }
+            Inspection insp = rest.getInspections().getInspection(position);
+            ImageView hazard_img = iv.findViewById(R.id.img_hazard);
+            TextView hazard_txt = iv.findViewById(R.id.txt_hazard);
+            TextView critissues_txt = iv.findViewById(R.id.txt_critIssues);
+            TextView Ncritissues_txt = iv.findViewById(R.id.txt_NcritIssues);
+            TextView inspDate_txt = iv.findViewById(R.id.txt_inspDate);
+            //setting stuff
+            critissues_txt.setText("# of Critical Issues: " + insp.getCritical());
+            Ncritissues_txt.setText("# of Non-Critical Issues: " + insp.getNonCritical());
 
+            switch (insp.getLevel()) {
+                case LOW:
+                    hazard_txt.setText(R.string.hazard_low);
+                    hazard_txt.setTextColor(Color.parseColor("#45DE08")); // Green
+                    hazard_img.setBackgroundResource(R.drawable.green_hazard);
+                    break;
+                case MODERATE:
+                    hazard_txt.setText(R.string.hazard_moderate);
+                    hazard_txt.setTextColor(Color.parseColor("#FA9009")); // Orange
+                    hazard_img.setBackgroundResource(R.drawable.orange_hazard);
+                    break;
+                case HIGH:
+                    hazard_txt.setText(R.string.hazard_high);
+                    hazard_txt.setTextColor(Color.parseColor("#FA2828")); // Red
+                    hazard_img.setBackgroundResource(R.drawable.red_hazard);
+                    break;
+            }
+            Date currentDate = new Date();
+            SimpleDateFormat formatter1 = new SimpleDateFormat("MMM yyyy");
+            long diffInMillies = Math.abs(currentDate.getTime() - insp.getDate().getTime());
+            long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+            String inspectionDateText;
+            if(diff > 365){
+                inspectionDateText = formatter1.format(insp.getDate());
+            } else if(diff > 30){
+                formatter1 = new SimpleDateFormat("MMM dd");
+                inspectionDateText = formatter1.format(insp.getDate());
+            } else{
+                inspectionDateText = diff + " days ago";
+            }
+            inspDate_txt.setText(inspectionDateText);
+            return iv;
         }
+    }
 
     private void setValues() {
         TextView ResName_txt = findViewById(R.id.txt_restname);
@@ -59,6 +111,13 @@ public class RestaurantDetails extends AppCompatActivity {
         ResAdd_txt.setText("Facility Location: \n"+rest.getAddress());
         TextView ResGps_txt = findViewById(R.id.txt_gps);
         ResGps_txt.setText(rest.getGPS());
+    }
+
+    private void OnClick() {
+        ListView insp_list = findViewById(R.id.list_insp);
+        insp_list.setOnItemClickListener(((parent, view, position, id) -> {
+            Toast.makeText(RestaurantDetails.this, "Open Inspection details activity for position: " + position, Toast.LENGTH_SHORT).show();
+        }));
     }
 
     private void Getdata() {
