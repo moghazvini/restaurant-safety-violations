5b92f5ec6cbd158e43408e860b9a15dc53b0565e
gloryq <gloryquan2@gmail.com>
2020-11-03 13:58:20 -0800

    finish Violation enum

--- app/src/main/java/ca/cmpt276/project/model/types/ViolationType.java
+++ app/src/main/java/ca/cmpt276/project/model/types/ViolationType.java
@@ -4,10 +4,16 @@ package ca.cmpt276.project.model.types;
  * Represents the type of Violation
  */
 public enum ViolationType {
-    APPROVAL,
-    TRAINING,
-    FOOD,
-    EQUIPMENT,
-    PESTS,
-    EMPLOYEES
+    OPERATOR("operator"),
+    FOOD("food"),
+    EQUIPMENT("equipment"),
+    PESTS("pests"),
+    EMPLOYEES("employees"),
+    ESTABLISHMENT("establishment violation");
+
+    public final String violation;
+
+    ViolationType(String violation) {
+        this.violation = violation;
+    }
 }
