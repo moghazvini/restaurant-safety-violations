24630d0852baa85c7d4053b24095a3105e3d86ca
gloryq <gloryquan2@gmail.com>
2020-11-03 14:11:54 -0800

    Complete Inspection Details tool bar

--- app/src/main/java/ca/cmpt276/project/ui/InspectionDetailsActivity.java
+++ app/src/main/java/ca/cmpt276/project/ui/InspectionDetailsActivity.java
@@ -1,22 +1,33 @@
 package ca.cmpt276.project.ui;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import ca.cmpt276.project.R;
 
 public class InspectionDetailsActivity extends AppCompatActivity {
 
-     private final static String INDEX = "Inspection Report Index";
+    private final static String INDEX = "Inspection Report Index";
+    private ActionBar back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspection_details);
+        Toolbar toolbar = findViewById(R.id.toolbar_inspection_det);
+        setSupportActionBar(toolbar);
+
+        // Enable "up" on toolbar
+        back = getSupportActionBar();
+        if (back != null) {
+            back.setDisplayHomeAsUpEnabled(true);
+        }
+
 
         Button backButton = findViewById(R.id.btn_back);
         backButton.setOnClickListener(v -> finish());
--- app/src/main/java/ca/cmpt276/project/ui/RestaurantDetailsActivity.java
+++ app/src/main/java/ca/cmpt276/project/ui/RestaurantDetailsActivity.java
@@ -13,7 +13,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Collections;
