89743b479906db9359f0401e58221b76e11708d7
Mo Ghaz <mo_ghazvini@sfu.ca>
2020-11-02 20:58:20 -0800

    new inpsection activity w/ basic layout

--- app/src/main/java/ca/cmpt276/project/ui/RestaurantListActivity.java
+++ app/src/main/java/ca/cmpt276/project/ui/RestaurantListActivity.java
@@ -214,9 +214,6 @@ public class RestaurantListActivity extends AppCompatActivity {
         ListView list = findViewById(R.id.listViewRestaurants);
         list.setOnItemClickListener((parent, viewClicked, position, id) -> {
 
-            Intent intent = new Intent(RestaurantListActivity.this, RestaurantDetails.class);
-            startActivity(intent);
-
             Toast.makeText(RestaurantListActivity.this, "Open Inspection list activity for position: " + position, Toast.LENGTH_SHORT).show();
             Intent i = RestaurantDetails.makeLaunchIntent(RestaurantListActivity.this,position);
             startActivity(i);
