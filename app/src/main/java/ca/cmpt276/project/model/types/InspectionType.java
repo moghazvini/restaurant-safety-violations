c1f23f599a008abb3a0af7f903bbf5b4691cf551
vaibhavs <vaibhavs@sfu.ca>
2020-11-04 21:03:09 -0800

    Finished Inspection details

--- app/src/main/java/ca/cmpt276/project/model/types/InspectionType.java
+++ app/src/main/java/ca/cmpt276/project/model/types/InspectionType.java
@@ -4,8 +4,8 @@ package ca.cmpt276.project.model.types;
  * Represents the inspection type.
  */
 public enum InspectionType {
-    ROUTINE("routine"),
-    FOLLOWUP("follow-up");
+    ROUTINE("Routine Check"),
+    FOLLOWUP("Follow-up Check");
 
     public final String value;
 
