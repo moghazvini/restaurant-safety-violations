1f295d170d2869a0950d2002768808638ad095e3
gloryq <gloryquan2@gmail.com>
2020-11-04 20:30:54 -0800

    Restore Inspection class code

--- app/src/main/java/ca/cmpt276/project/model/Inspection.java
+++ app/src/main/java/ca/cmpt276/project/model/Inspection.java
@@ -12,7 +12,7 @@ import ca.cmpt276.project.model.types.InspectionType;
 import ca.cmpt276.project.model.types.Severity;
 
 /**
- * Represents an inspection report and
+ * Represents an inpsection report and
  * contains a list of violations.
  */
 public class Inspection implements Comparable<Inspection>{
@@ -23,29 +23,13 @@ public class Inspection implements Comparable<Inspection>{
     private HazardLevel level;
     private List<Violation> violations;
 
-    public Inspection(Date date, InspectionType type, int critical, int nonCritical, HazardLevel level, String vioLump) {
+    public Inspection(Date date, InspectionType type, int critical, int nonCritical, HazardLevel level) {
         this.date = date;
         this.type = type;
         this.critical = critical;
         this.nonCritical = nonCritical;
         this.level = level;
         violations = new ArrayList<>();
-        fillViolation(vioLump);
-    }
-
-    private void fillViolation(String lump) {
-        // parse the lump to extract the info
-        String[] info = lump.split(",");
-        int code = Integer.parseInt(info[0]);
-
-        Severity severity;
-        if (info[1].equals("Not Critical")) {
-            severity = Severity.NOTCRITICAL;
-        } else {
-            severity = Severity.CRITICAL;
-        }
-
-        violations.add(new Violation(code,severity,info[2],info[3]));
     }
 
     public Inspection(Date date, InspectionType type, int critical, int nonCritical, HazardLevel level, String vioLump) {
