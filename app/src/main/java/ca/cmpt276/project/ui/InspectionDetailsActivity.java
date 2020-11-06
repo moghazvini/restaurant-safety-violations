c4f24b2ebab47b34d950351177b710737f4a53e0
gloryq <gloryquan2@gmail.com>
2020-11-06 00:33:19 -0800

    Fix indentation

--- app/src/main/java/ca/cmpt276/project/ui/InspectionDetailsActivity.java
+++ app/src/main/java/ca/cmpt276/project/ui/InspectionDetailsActivity.java
@@ -30,7 +30,7 @@ import ca.cmpt276.project.model.RestaurantListManager;
 import ca.cmpt276.project.model.Violation;
 
 /**
- *  used to display details of a single inspection
+ *  Displays details of a single inspection
  */
 public class InspectionDetailsActivity extends AppCompatActivity {
 
@@ -63,141 +63,142 @@ public class InspectionDetailsActivity extends AppCompatActivity {
             populateListView();
             onClick();
 
-        }
+    }
 
-        private void populateListView() {
+    private void populateListView() {
 
-            Collections.sort(restaurantManager.getList());
-            ListView listView = findViewById(R.id.listView);
+        Collections.sort(restaurantManager.getList());
+        ListView listView = findViewById(R.id.listView);
 
-            ArrayAdapter<Violation> arrayAdapter = new ViolationListAdapter();
-            TextView noViolations = findViewById(R.id.empty_violations);
-            listView.setEmptyView(noViolations);
-            
-            listView.setAdapter(arrayAdapter);
-        }
+        ArrayAdapter<Violation> arrayAdapter = new ViolationListAdapter();
+        TextView noViolations = findViewById(R.id.empty_violations);
+        listView.setEmptyView(noViolations);
 
-        private class ViolationListAdapter extends ArrayAdapter<Violation> {
-                    public ViolationListAdapter () {
-                        super(InspectionDetailsActivity.this, R.layout.violation_list, inspection.getViolations());
-                    }
+        listView.setAdapter(arrayAdapter);
+    }
 
-            @NonNull
-            @Override
-            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-                View itemView = convertView;
-                if(itemView == null){
-                    itemView = getLayoutInflater().inflate(R.layout.violation_list, parent, false);
+    private class ViolationListAdapter extends ArrayAdapter<Violation> {
+                public ViolationListAdapter () {
+                    super(InspectionDetailsActivity.this, R.layout.violation_list, inspection.getViolations());
                 }
 
-                Violation violation = inspection.getViolation(position);
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View itemView = convertView;
+            if(itemView == null){
+                itemView = getLayoutInflater().inflate(R.layout.violation_list, parent, false);
+            }
 
-                ImageView violation_img = itemView.findViewById(R.id.img_hazard);
-                TextView brief_description_txt = itemView.findViewById(R.id.txt_critissues);
-                TextView crit_or_not_txt = itemView.findViewById(R.id.txt_critical_or_not);
+            Violation violation = inspection.getViolation(position);
 
-                if(inspectionManager.getInspections().size()>0) {
-                    String brief_description = violation.getCode() +"  "+ violation.getType().violation;
-                    String crit_or_not = violation.getSeverity().severity;
-                    brief_description_txt.setText(brief_description);
-                    if(crit_or_not.equals("Critical")){
-                        crit_or_not_txt.setTextColor(Color.parseColor("#FA2828")); // Red
-                    }else {
-                        crit_or_not_txt.setTextColor(Color.parseColor("#45DE08")); // Green
-                    }
-                    crit_or_not_txt.setText(crit_or_not);
+            ImageView violation_img = itemView.findViewById(R.id.img_hazard);
+            TextView brief_description_txt = itemView.findViewById(R.id.txt_critissues);
+            TextView crit_or_not_txt = itemView.findViewById(R.id.txt_critical_or_not);
 
-                    switch (violation.getType()) {
-                        case OPERATOR:
-                            violation_img.setBackgroundResource(R.drawable.non_crit_operator_violations);
-                            break;
-                        case FOOD:
-                            violation_img.setBackgroundResource(R.drawable.crit_food_violations);
-                            break;
-                        case EQUIPMENT:
-                            violation_img.setBackgroundResource(R.drawable.crit_equiptment_violations);
-                            break;
-                        case PESTS:
-                            violation_img.setBackgroundResource(R.drawable.non_crit_pest_violations);
-                            break;
-                        case EMPLOYEES:
-                            violation_img.setBackgroundResource(R.drawable.non_crit_employee_violations);
-                            break;
-                        case ESTABLISHMENT:
-                            violation_img.setBackgroundResource(R.drawable.crit_employees_violations);
-                            break;
-                        case CHEMICAL:
-                            violation_img.setBackgroundResource(R.drawable._09ncrit_chems);
-                            break;
-                        default:
-                            assert false;
-                    }
+            if(inspectionManager.getInspections().size()>0) {
+                String brief_description = violation.getCode() +"  "+ violation.getType().violation;
+                String crit_or_not = violation.getSeverity().severity;
+                brief_description_txt.setText(brief_description);
+                if(crit_or_not.equals("Critical")){
+                    crit_or_not_txt.setTextColor(Color.parseColor("#FA2828")); // Red
+                }else {
+                    crit_or_not_txt.setTextColor(Color.parseColor("#45DE08")); // Green
                 }
-                else {
-                    brief_description_txt.setText(R.string.no_inspection_found);
-                    crit_or_not_txt.setText(R.string.no_inspection_found);
+                crit_or_not_txt.setText(crit_or_not);
+
+                switch (violation.getType()) {
+                    case OPERATOR:
+                        violation_img.setBackgroundResource(R.drawable.non_crit_operator_violations);
+                        break;
+                    case FOOD:
+                        violation_img.setBackgroundResource(R.drawable.crit_food_violations);
+                        break;
+                    case EQUIPMENT:
+                        violation_img.setBackgroundResource(R.drawable.crit_equiptment_violations);
+                        break;
+                    case PESTS:
+                        violation_img.setBackgroundResource(R.drawable.non_crit_pest_violations);
+                        break;
+                    case EMPLOYEES:
+                        violation_img.setBackgroundResource(R.drawable.non_crit_employee_violations);
+                        break;
+                    case ESTABLISHMENT:
+                        violation_img.setBackgroundResource(R.drawable.crit_employees_violations);
+                        break;
+                    case CHEMICAL:
+                        violation_img.setBackgroundResource(R.drawable._09ncrit_chems);
+                        break;
+                    default:
+                        assert false;
                 }
-                return itemView;
             }
+            else {
+                brief_description_txt.setText(R.string.no_inspection_found);
+                crit_or_not_txt.setText(R.string.no_inspection_found);
+            }
+            return itemView;
+        }
+    }
+
+    private void setValues() {
+        LocalDate currentDate = LocalDate.now();
+        String inspectionDateText;
+        if(Math.abs(currentDate.getYear() - inspection.getDate().getYear()) != 0){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM yyyy");
+            inspectionDateText = formatter.format(inspection.getDate());
+        }
+        else if(Math.abs(currentDate.getMonthValue() - inspection.getDate().getMonthValue()) != 0){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd");
+            inspectionDateText = formatter.format(inspection.getDate());
+        }
+        else{
+            inspectionDateText = inspection.getDate().getDayOfMonth() + " days ago";
         }
 
-        private void setValues() {
-            LocalDate currentDate = LocalDate.now();
-            String inspectionDateText;
-            if(Math.abs(currentDate.getYear() - inspection.getDate().getYear()) != 0){
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM yyyy");
-                inspectionDateText = formatter.format(inspection.getDate());
-            }
-            else if(Math.abs(currentDate.getMonthValue() - inspection.getDate().getMonthValue()) != 0){
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd");
-                inspectionDateText = formatter.format(inspection.getDate());
-            }
-            else{
-                inspectionDateText = inspection.getDate().getDayOfMonth() + " days ago";
-            }
+        TextView inspectionType_txt = findViewById(R.id.txt_inspection_type);
+        TextView critissues = findViewById(R.id.txt_critissues);
+        TextView Ncritissues = findViewById(R.id.txt_num_non_crtitical);
 
-            TextView inspectionType_txt = findViewById(R.id.txt_inspection_type);
-            TextView critissues = findViewById(R.id.txt_critissues);
-            TextView Ncritissues = findViewById(R.id.txt_num_non_crtitical);
+        back.setTitle(inspectionDateText);
+        String type = inspection.getType().value;
+        inspectionType_txt.setText(type);
+        String nonCrit = "# of Critical Issues: " + inspection.getCritical();
+        String crit = "# of Non-Critical Issues: " + inspection.getNonCritical();
 
-            back.setTitle(inspectionDateText);
-            String type = inspection.getType().value;
-            inspectionType_txt.setText(type);
-            String nonCrit = "# of Critical Issues: " + inspection.getCritical();
-            String crit = "# of Non-Critical Issues: " + inspection.getNonCritical();
+        critissues.setText(nonCrit);
+        Ncritissues.setText(crit);
 
-            critissues.setText(nonCrit);
-            Ncritissues.setText(crit);
-
-            TextView hazard_txt = findViewById(R.id.txt_hazard);
-            ImageView hazard_img = findViewById(R.id.img_hazard);
-            switch (inspection.getLevel()) {
-                case LOW:
-                    hazard_txt.setText(R.string.hazard_low);
-                    hazard_txt.setTextColor(Color.parseColor("#45DE08")); // Green
-                    hazard_img.setBackgroundResource(R.drawable.green_hazard);
-                    break;
-                case MODERATE:
-                    hazard_txt.setText(R.string.hazard_moderate);
-                    hazard_txt.setTextColor(Color.parseColor("#FA9009")); // Orange
-                    hazard_img.setBackgroundResource(R.drawable.orange_hazard);
-                    break;
-                case HIGH:
-                    hazard_txt.setText(R.string.hazard_high);
-                    hazard_txt.setTextColor(Color.parseColor("#FA2828")); // Red
-                    hazard_img.setBackgroundResource(R.drawable.red_hazard);
-                    break;
-                default:
-                    assert false;
-            }
-
+        TextView hazard_txt = findViewById(R.id.txt_hazard);
+        ImageView hazard_img = findViewById(R.id.img_hazard);
+        switch (inspection.getLevel()) {
+            case LOW:
+                hazard_txt.setText(R.string.hazard_low);
+                hazard_txt.setTextColor(Color.parseColor("#45DE08")); // Green
+                hazard_img.setBackgroundResource(R.drawable.green_hazard);
+                break;
+            case MODERATE:
+                hazard_txt.setText(R.string.hazard_moderate);
+                hazard_txt.setTextColor(Color.parseColor("#FA9009")); // Orange
+                hazard_img.setBackgroundResource(R.drawable.orange_hazard);
+                break;
+            case HIGH:
+                hazard_txt.setText(R.string.hazard_high);
+                hazard_txt.setTextColor(Color.parseColor("#FA2828")); // Red
+                hazard_img.setBackgroundResource(R.drawable.red_hazard);
+                break;
+            default:
+                assert false;
         }
 
-        private void onClick() {
-            ListView list = findViewById(R.id.listView);
-            list.setOnItemClickListener((parent, viewClicked, position, id) -> Toast.makeText(InspectionDetailsActivity.this, inspection.getViolation(position).getLongDis(), Toast.LENGTH_LONG).show());
-        }
-        public static Intent makeLaunchIntent(RestaurantDetailsActivity restaurantDetails, int position, int rest_position) {
+    }
+
+    private void onClick() {
+        ListView list = findViewById(R.id.listView);
+        list.setOnItemClickListener((parent, viewClicked, position, id) -> Toast.makeText(InspectionDetailsActivity.this, inspection.getViolation(position).getLongDis(), Toast.LENGTH_LONG).show());
+    }
+
+    public static Intent makeLaunchIntent(RestaurantDetailsActivity restaurantDetails, int position, int rest_position) {
         Intent intent = new Intent(restaurantDetails, InspectionDetailsActivity.class);
         intent.putExtra(INDEX, position);
         intent.putExtra(REST_INDEX, rest_position);
